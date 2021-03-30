8699c1d91a424b7a6f8e7e594d55f50eb0f58249
Tim Hanewich <tahanewich@live.com>
2021-03-30 13:47:25 -0400

    The get data from label method now uses the found appropriate context, not the stated primary

--- src/Helpers/XbrlInstanceDocumentHelper.cs
+++ src/Helpers/XbrlInstanceDocumentHelper.cs
@@ -7,11 +7,14 @@ namespace Xbrl.Helpers
     {
         public static XbrlFact GetValueFromPriorities(this XbrlInstanceDocument doc, params string[] priorities)
         {
+            //First get the normal period context
+            string normal_period_context = doc.FindNormalPeriodPrimaryContext().Id;
+
             foreach (string word in priorities)
             {
                 foreach (XbrlFact fact in doc.Facts)
                 {
-                    if (fact.ContextId.ToLower().Trim() == doc.PrimaryInstantContextId.ToLower().Trim() || fact.ContextId.Trim().ToLower() == doc.PrimaryPeriodContextId.Trim().ToLower())
+                    if (fact.ContextId.ToLower().Trim() == doc.PrimaryInstantContextId.ToLower().Trim() || fact.ContextId.Trim().ToLower() == normal_period_context.Trim().ToLower())
                     {
                         if (fact.Label.Trim().ToLower() == word.Trim().ToLower())
                         {
