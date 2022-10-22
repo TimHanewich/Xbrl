1c9173e4ba9c49f4716ea6a053393723add5bce1
wyattschwanbeck <wyattschwanbeck@gmail.com>
2022-10-22 14:47:02 -0400

    Revert "Final commit to match master branch."
    This reverts commit b15f598f20d4ef6587e5b36d6789d9f4c904de6f, reversing
    changes made to ee5fde1f98e7dde991b82476309f6d854d5a0e08.

--- src/XbrlInstanceDocument.cs
+++ src/XbrlInstanceDocument.cs
@@ -96,7 +96,7 @@ namespace Xbrl
                     fact.Label = node.Name.Contains(":") ? node.Name.Split(':')[1] : node.Name;
                     //Populate instance document trading symbol and Type
                     if (fact.Label == "TradingSymbol")
-                        ToReturn.TradingSymbol = fact.Value;
+                        ToReturn.TradingSymbol = fact.Label;
                     else if (fact.Label == "DocumentType")
                         ToReturn.DocumentType = fact.Value;
 
